b947fa180caccf4ca7081f87e6c5ef3318e0aba0
Bill Dimou <vasileiosdimou@gmail.com>
2022-06-14 17:08:00 +0300

    app structure update - blueprints (v0.15)

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')	
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	POSTS_PER_PAGE = 3
+	POSTS_PER_PAGE = 10
 	LANGUAGES = ['en','es']
 	MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
 	
