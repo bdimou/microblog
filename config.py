b5eda27d72cf4f3200458161e82087c71849dda4
Bill Dimou <vasileiosdimou@gmail.com>
2022-06-20 23:19:54 +0300

    full-text search (v0.16)

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config(object):
 	POSTS_PER_PAGE = 10
 	LANGUAGES = ['en','es']
 	MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+	ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 	
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
