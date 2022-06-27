3f1f04905493d06b0505f0051ce981b4af81f37c
Bill Dimou <vasileiosdimou@gmail.com>
2022-06-27 14:27:57 +0300

    user notifications (v0.18)

---
+++ migrations/versions/3d2c9412cc68_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 3d2c9412cc68
+Revises: be243906f6b5
+Create Date: 2022-06-23 18:06:03.514258
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d2c9412cc68'
+down_revision = 'be243906f6b5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
