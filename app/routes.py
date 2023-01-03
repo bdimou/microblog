82ef74d43b726e43c588e20227c4880395907e1e
Bill Dimou <vasileiosdimou@gmail.com>
2023-01-03 12:02:20 +0000

    Pagination (v0.9)

--- app/routes.py
+++ app/routes.py
@@ -8,26 +8,27 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app,db
-from app.forms import LoginForm, RegistrationForm,EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm,EditProfileForm, EmptyForm, PostForm
+from app.models import User,Post
 
 
-@app.route('/')                # A decorator modifies the function that follows it. A common pattern with decorators is to use them to register functions as callbacks for certain events
-@app.route('/index')           # In this case, the @app.route decorator creates an association between the URL given as an argument and the function 
+@app.route('/', methods =['GET','POST'])                # A decorator modifies the function that follows it. A common pattern with decorators is to use them to register functions as callbacks for certain events
+@app.route('/index',methods =['GET','POST'])            # In this case, the @app.route decorator creates an association between the URL given as an argument and the function 
 @login_required
 def index():
-    user = {'username':'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html',title='Home',posts=posts) # this will call Jinja2 (template engine) to handle dynamic content placeholders
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author =current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))               # Post/Redirect/Get pattern. It avoids inserting posts when a user inadvertently refreshes the pages after submitting a web form.
+    page = request.args.get('page',1,type =int)
+    posts = current_user.followed_posts().paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out = False)
+    next_url = url_for('index',page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index',page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html',title='Home',form=form, posts=posts.items,next_url = next_url, prev_url = prev_url) # this will call Jinja2 (template engine) to handle dynamic content placeholders
     
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -70,12 +71,13 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author':user,'body':'Test post #1'},
-        {'author':user,'body':'Test post #2'}
-    ]
+    page = request.args.get('page',1,type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page,per_page=app.config['POSTS_PER_PAGE'], error_out=False)
     form = EmptyForm()
-    return render_template('user.html',user=user,posts=posts,form=form)
+    next_url = url_for('user',username = user.username, page= posts.next_num) if posts.has_next else None
+    prev_url = url_for('user',username= user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html',user=user,posts=posts.items,next_url=next_url, prev_url =prev_url,form=form)
 
 @app.route('/edit_profile', methods =['GET','POST'])
 @login_required
@@ -136,3 +138,12 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page',1,type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page,per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore',page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore',page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html',title='Explore',posts=posts.items,next_url = next_url, prev_url=prev_url)
