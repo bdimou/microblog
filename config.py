4e25ced5338be2bf6fd50f996e06843f47d3f674
Bill Dimou <vasileiosdimou@gmail.com>
2023-01-27 20:43:39 +0000

    User Pop-ups (v0.17)

--- config.py
+++ config.py
@@ -21,10 +21,11 @@ class Config(object):
     ELASTICSEARCH_PW = os.environ.get('ELASTICSEARCH_PW')
     ELASTICSEARCH_CERT_PATH = os.environ.get('ELASTICSEARCH_CERT_PATH')
 
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = os.environ.get('ADMINS')[0]
+    ADMINS = os.environ.get('ADMINS')
     
